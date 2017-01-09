e4aa1b3f854cba506fdab20b4eb2bb74bb9c585d
ThiagoLopes <thiagolopes@protonmail.com>
2017-01-08 23:57:41 -0200

    mais umas mudanças no scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp = require('gulp'),
     browsersync = require ('browser-sync').create(),
     reloed = browsersync.reload,
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
- 
+    sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    gulpclean = require('gulp-clean'),
+    cssmin = require('gulp-cssmin');
+
 gulp.task('default',['browserSyncWatcher'],function(){
 
 });
@@ -14,16 +17,27 @@ gulp.task('browserSyncWatcher', function(){
             baseDir: '.'
         }
     });
-    
-    gulp.watch(['./src/sass/**/*.scss'], ['sass']);
+
+    gulp.watch(['./**/*.scss'], ['sass']);
     gulp.watch(['./*.html'], reloed);
 });
 
 gulp.task('sass', function(){
     return gulp.src("./src/sass/**/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(browsersync.stream());
-})
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest("./dist/css"))
+        .pipe(browsersync.stream());
+});
+
+gulp.task('clean-img-folder',function(){
+    return gulp.src('./dist/img')
+        .pipe(gulpclean());
+});
+
+gulp.task('make-img', ['clean-img-folder'],function(){
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+});
