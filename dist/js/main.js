7dc74153ee743350eb730afceb9d32049435c0f8
ThiagoLopes <thiagolopes@protonmail.com>
2017-01-15 18:59:21 -0200

    test inspeclet

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,21 @@
 $(function(){
+    window.__insp = window.__insp || [];
+    __insp.push(['wid', 2011419361]);
+    (function() {
+        function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+        setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+    })();
 });
 
 //function exibirNomesLink(){
-    //var links = $('.social').find('a');
-    //var content = [];
-    //content.push = $(links).find('span').text();
+//var links = $('.social').find('a');
+//var content = [];
+//content.push = $(links).find('span').text();
 
-    //$(links).tooltipster({
-        //theme: 'tooltipster-shadow',
-        //delay: 0
-    //});
+//$(links).tooltipster({
+//theme: 'tooltipster-shadow',
+//delay: 0
+//});
 //};
 
 //function criarPanel(objList){
